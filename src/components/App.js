fd7b0c1a14b7315c7a69a5f2bbaa6882d596d578
Aasif Dange <97660014+Aasi555@users.noreply.github.com>
2022-02-21 12:36:17 +0530

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import '../styles/App.css';
+import "./styles.css";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/Landing";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Welcome from "./components/Welcome";
 
-
-const App = () => {
-  
+export default function App() {
   return (
-
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/welcome" element={<Welcome />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-
-export default App;
